bc82f5320d312f1c625eac581cbaa339a1ecf5bd
Arvin-Tony <arvintonyk@gmail.com>
2024-09-20 10:00:43 -0400

    First Commit

---
+++ src/notes_racer.js
@@ -0,0 +1,17 @@
+//TypingGame.js
+import React, {
+    useState,
+    useEffect
+} from 'react';
+import './App.css';
+
+
+const TypingGame = () => {
+    return (
+        <div>
+            <h1>Typing Game.</h1>
+        </div>
+    )
+};
+
+export default TypingGame;
